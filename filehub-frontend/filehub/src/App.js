d5bf8b98267e58a8be6afdf92f5c4e985f6fcfc8
upasanakumar <upasana.kumar@sjsu.edu>
2024-03-24 19:38:27 -0700

    Add UI

--- filehub-frontend/filehub/src/App.js
+++ filehub-frontend/filehub/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log("Login button clicked!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={handleLogin} className="App-link">
+            Click to Login
+          </button>
+        </header>
+      </div>
   );
 }
 
