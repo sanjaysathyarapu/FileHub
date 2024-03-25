f088153f89c0f3ef483b61bdc3e6716695bebb5b
sanjaysathyarapu <143290734+sanjaysathyarapu@users.noreply.github.com>
2024-03-24 18:54:59 -0700

    Create terraform.tf

---
+++ terraform/terraform.tf
@@ -0,0 +1,19 @@
+
+# Define AWS provider
+provider "aws" {
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+# Define an AWS S3 bucket
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = var.bucket_name
+  acl = "private"
+}
+
+# Define an AWS EC2 instance
+resource "aws_instance" "example_instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+}
